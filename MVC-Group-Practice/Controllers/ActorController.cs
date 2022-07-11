64b9511943fc0527c38f70f6358e0ed3e3848c74
Jaesung Hong <jezsung@gmail.com>
2022-07-11 13:25:17 -0500

    Part 1 Step 2

---
+++ MVC-Group-Practice/Controllers/ActorController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MVC_Group_Practice.Models;
+
+namespace MVC_Group_Practice.Controllers
+{
+    public class ActorController : Controller
+    {
+        private MovieDatabaseContext _context;
+
+        public ActorController(MovieDatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Actor> actors = _context.Actors.ToList();
+            return View(actors);
+        }
+    }
+}
