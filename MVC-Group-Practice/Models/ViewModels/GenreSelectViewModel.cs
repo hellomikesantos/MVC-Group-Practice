de3e77d12b6efb4baf3ed259a2bead1a732b7f9c
Michael John Santos <michaeljohnsantos@student.mitt.ca>
2022-07-12 12:43:42 -0500

    Add Genre Dropdown Page

---
+++ MVC-Group-Practice/Models/ViewModels/GenreSelectViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace MVC_Group_Practice.Models.ViewModels
+{
+    public class GenreSelectViewModel
+    {
+        public List<SelectListItem> GenreSelectItems { get; set; } = new List<SelectListItem>();
+        public List<Movie> MovieList { get; set; } 
+        public GenreSelectViewModel(List<Genre> genres)
+        {
+            genres.ForEach(genre =>
+            {
+                GenreSelectItems.Add(new SelectListItem(genre.Name, genre.Id.ToString()));
+            });
+        }
+    }
+}
